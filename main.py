0bf7e7e029b9cfb7f299b85b152ae370f17f3bd6
Antti Viitala <antti.viitala@icloud.com>
2022-02-27 15:03:18 +0800

    Separate function for upper body detection

--- main.py
+++ main.py
@@ -20,6 +20,24 @@ def convertToRGB(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 haar_cascade_face = cv2.CascadeClassifier('data/haarcascades/haarcascade_frontalface_default.xml')
+haar_cascade_upperbody = cv2.CascadeClassifier('data/haarcascades/haarcascade_upperbody.xml')
+
+def detect_upperbody(cascade, test_image, scaleFactor = 1.2):
+    # create a copy of the image to prevent any changes to the original one.
+    image_copy = test_image.copy()
+
+    #convert the test image to gray scale as opencv face detector expects gray images
+    gray_image = cv2.cvtColor(image_copy, cv2.COLOR_BGR2GRAY)
+    gray_image = np.array(gray_image, dtype='uint8')
+
+    # Applying the haar classifier to detect faces
+    shoulders_rect = cascade.detectMultiScale(gray_image, scaleFactor=scaleFactor, minNeighbors=4)
+
+    area = 0
+    for (x, y, w, h) in shoulders_rect:
+        area = (w/100.0) * (h/100.0)
+        cv2.rectangle(image_copy, (x, y), (x+w, y+h), (255, 0, 0), 3)
+    return image_copy, area
 
 def detect_faces(cascade, test_image, scaleFactor = 1.1, threshold=False):
     # create a copy of the image to prevent any changes to the original one.
@@ -41,6 +59,27 @@ def detect_faces(cascade, test_image, scaleFactor = 1.1, threshold=False):
             cv2.rectangle(image_copy, (x, y), (x+w, y+h), (0, 255, 0), 3)
         
     return image_copy, area
+
+cap = cv2.VideoCapture(0)
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #do face detection
+    shoulders_detect, area = detect_upperbody(haar_cascade_upperbody, frame)
+
+    # Display the resulting frame
+    cv2.imshow('frame',shoulders_detect)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
+
 
 
 if __name__ == '__main__':
